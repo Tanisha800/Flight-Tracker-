6eb673e6ecd0fbe4604f87aa3fc06dbfd162d93d
tanisha800 <tanisha@Tanishas-MacBook-Pro.local>
2025-05-15 22:31:54 +0530

    services hero section added

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Services.css';
+function Services(){
+    return(
+        <div className="services-setion">
+        <div className='overlay'>
+        <h2 className='title'>Our Services</h2>
+        <p className="subtitle">
+            Discover our comprehensive suite of flight tracking and aviation information services
+        </p>
+        </div>    
+        </div>    
+          
+
+    )
+
+}
+export default Services;
