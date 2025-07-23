322add3dfbd89606cba57b533c891b3046119d33
tanisha800 <tanisha@Tanishas-MacBook-Pro.local>
2025-07-23 09:33:45 +0530

    Minor fixes

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import Services from '../components/services/services'
-import FAQsection from '../components/FAQsection/FAQsection'
-import Tracker from '../components/Tracker/Tracker'
-import Route from '../components/Route/Route'
+import React from "react";
 
+import FAQsection from "../components/FAQsection/FAQsection";
+import Tracker from "../components/Tracker/Tracker";
+import Route from "../components/Route/Route";
+import Services from "../components/services/services";
 
 function Service() {
-
   return (
     <div>
-        <Services/>
-        <Tracker/>
-        <Route/>
-        <FAQsection/>
-
-        
+      <Services />
+      <Tracker />
+      <Route />
+      <FAQsection />
     </div>
-  )
+  );
 }
 
-
-export default Service
+export default Service;
